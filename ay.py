1fba685325b0a0281e5218eca2af13531d06feb1
lyh80972490 <3137766747@qq.com>
2020-10-15 08:31:38 +0800

    ay

--- ay.py
+++ ay.py
@@ -1,23 +1,8 @@
 import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
-import math
- 
-size = 300
-new_im = Image.new("RGBA",(size,size)) #创建一个空的图片
- 
-a_img = np.array(new_im)  #获取空图片的数据
- 
-m = (size-50)/2 #函数的摆幅
-for i in range(0,size):
-    x = 2 * math.pi * (  i / size )  #控制 根据 i / size 的比例，控制 x 的值为  0-2π
-    y = size/2 - m*math.sin(x)  #将正弦函数画在中间
-    y = int(y)
-    if y < size and y >= 0:
-        a_img[y][i] = (0,0,0,255)   
- 
- 
-plt.figure("beauty")
-plt.imshow(a_img)
-#plt.axis('off')
-plt.show()
+import scipy as sp
+import pylab as pl
+x=np.linspace(0,4*np.pi,100)
+
+pl.plot(x,pl.sin(x))
+
+pl.show()
